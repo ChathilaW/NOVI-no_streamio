7422669e97a2765782fade2b9b585fc52daf5787
Chathila_Wijesinghe <chathilawijesinghe@gmail.com>
2026-02-18 23:01:10 +0530

    Update page.tsx

--- novi-app/app/(main)/(home)/upcoming/page.tsx
+++ novi-app/app/(main)/(home)/upcoming/page.tsx
@@ -1,16 +1,10 @@
-import CallList from "@/components/CallList";
-
-
 const upcoming =()=>{
-    return(
-        <section className="flex size-full flex-col gap-10 animate-fade-in">
-            <h1 className="text-3xl text-black text-center mt-3">upcoming meetings
-
-            </h1>
-            <CallList type="upcoming"/>
-
+    return (
+        <section className="flex size-full flex-col gap-10 pt-20 text-white animate-fade-in">
+            <h1 className="text-3xl text-black text-center mt-3">Under Development....</h1>
         </section>
-    );
+        
+    )
 
 
 };
